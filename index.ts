87e2eb4d51bf09dabe69454b9e25bfdaeabe425f
Ian Copp <copp.ian@gmail.com>
2016-12-29 12:37:56 -0800

    Update export to deal with Webpack weirdness

--- index.ts
+++ index.ts
@@ -6,13 +6,45 @@
  * @see https://github.com/cowboy/javascript-debug
  */
 
-export class Travelpen {
-  private _logLevel: number = 9
-  private _logCallback: Function | undefined = undefined
-  private _forceLogCallback: boolean = false
-  private _logs: any[] = []
+export interface Travelpen {
+  _logLevel: number
+  _logCallback: Function | undefined
+  _forceLogCallback: boolean
+  _logs: any[]
 
-  setCallback (callback: (logLevel: number, ...optionalParams: any[]) => void, force?: boolean, limit?: number) {
+  setCallback (callback: (logLevel: number, ...optionalParams: any[]) => void, force?: boolean, limit?: number): void
+  setLevel (level: number): void
+
+  assert (test?: boolean, message?: string, ...optionalParams: any[]): void
+  clear (): void
+  count (countTitle?: string): void
+  dir (value?: any, ...optionalParams: any[]): void
+  dirxml (value: any): void
+  exception (message?: string, ...optionalParams: any[]): void
+  group (groupTitle?: string): void
+  groupCollapsed (groupTitle?: string): void
+  groupEnd (): void
+  profile (reportName?: string): void
+  profileEnd (): void
+  table (...data: any[]): void
+  time (timerName?: string): void
+  timeEnd (timerName?: string): void
+  trace (message?: any, ...optionalParams: any[]): void
+
+  log (message?: any, ...optionalParams: any[]): void
+  debug (message?: any, ...optionalParams: any[]): void
+  info (message?: any, ...optionalParams: any[]): void
+  warn (message?: any, ...optionalParams: any[]): void
+  error (message?: any, ...optionalParams: any[]): void
+}
+
+let travelpen: Travelpen = {
+  _logLevel: 9,
+  _logCallback: undefined,
+  _forceLogCallback: false,
+  _logs: [],
+
+  setCallback (callback: (logLevel: number, ...optionalParams: any[]) => void, force?: boolean, limit?: number): void {
     this._forceLogCallback = force || false
     this._logCallback = callback || undefined
     limit = typeof limit === 'number' ? limit : this._logs.length
@@ -20,11 +52,11 @@ export class Travelpen {
     for (let i = Math.max(0, this._logs.length - limit); i < this._logs.length; i++) {
       this._logCallback.apply(this, this._logs[i])
     }
-  }
+  },
 
-  setLevel (level: number) {
+  setLevel (level: number): void {
     this._logLevel = typeof level === 'number' ? level : 9
-  }
+  },
 
   //////////////////////////////////////////////////////////////////////////////
   // Pass-through methods
@@ -34,91 +66,91 @@ export class Travelpen {
     if (this._logLevel !== 0 && console && console.assert) {
       console.assert.apply(console, arguments)
     }
-  }
+  },
 
   clear (): void {
     if (this._logLevel !== 0 && console && console.clear) {
       console.clear.apply(console, arguments)
     }
-  }
+  },
 
   count (countTitle?: string): void {
     if (this._logLevel !== 0 && console && console.count) {
       console.count.apply(console, arguments)
     }
-  }
+  },
 
   dir (value?: any, ...optionalParams: any[]): void {
     if (this._logLevel !== 0 && console && console.dir) {
       console.dir.apply(console, arguments)
     }
-  }
+  },
 
   dirxml (value: any): void {
     if (this._logLevel !== 0 && console && console.dirxml) {
       console.dir.apply(console, arguments)
     }
-  }
+  },
 
   exception (message?: string, ...optionalParams: any[]): void {
     if (this._logLevel !== 0 && console && console.exception) {
       console.exception.apply(console, arguments)
     }
-  }
+  },
 
   group (groupTitle?: string): void {
     if (this._logLevel !== 0 && console && console.group) {
       console.group.apply(console, arguments)
     }
-  }
+  },
 
   groupCollapsed (groupTitle?: string): void {
     if (this._logLevel !== 0 && console && console.groupCollapsed) {
       console.groupCollapsed.apply(console, arguments)
     }
-  }
+  },
 
   groupEnd (): void {
     if (this._logLevel !== 0 && console && console.groupEnd) {
       console.groupEnd.apply(console, arguments)
     }
-  }
+  },
 
   profile (reportName?: string): void {
     if (this._logLevel !== 0 && console && console.profile) {
       console.profile.apply(console, arguments)
     }
-  }
+  },
 
   profileEnd (): void {
     if (this._logLevel !== 0 && console && console.profileEnd) {
       console.profileEnd.apply(console, arguments)
     }
-  }
+  },
 
   table (...data: any[]): void {
     if (this._logLevel !== 0 && console && console.table) {
       console.table.apply(console, arguments)
     }
-  }
+  },
 
   time (timerName?: string): void {
     if (this._logLevel !== 0 && console && console.time) {
       console.time.apply(console, arguments)
     }
-  }
+  },
 
   timeEnd (timerName?: string): void {
     if (this._logLevel !== 0 && console && console.timeEnd) {
       console.timeEnd.apply(console, arguments)
     }
-  }
+  },
 
   trace (message?: any, ...optionalParams: any[]): void {
     if (this._logLevel !== 0 && console && console.trace) {
       console.trace.apply(console, arguments)
     }
-  }
+  },
 
   //////////////////////////////////////////////////////////////////////////////
   // Logging methods
@@ -136,7 +168,7 @@ export class Travelpen {
         console.log.apply(console, arguments)
       }
     }
-  }
+  },
 
   debug (message?: any, ...optionalParams: any[]) {
     this._logs.push([2, ...Array.prototype.slice.call(arguments)])
@@ -152,7 +184,7 @@ export class Travelpen {
         console.log.apply(console, arguments)
       }
     }
-  }
+  },
 
   info (message?: any, ...optionalParams: any[]) {
     this._logs.push([3, ...Array.prototype.slice.call(arguments)])
@@ -168,7 +200,7 @@ export class Travelpen {
         console.log.apply(console, arguments)
       }
     }
-  }
+  },
 
   warn (message?: any, ...optionalParams: any[]) {
     this._logs.push([4, ...Array.prototype.slice.call(arguments)])
@@ -184,7 +216,7 @@ export class Travelpen {
         console.log.apply(console, arguments)
       }
     }
-  }
+  },
 
   error (message?: any, ...optionalParams: any[]) {
     this._logs.push([5, ...Array.prototype.slice.call(arguments)])
@@ -202,7 +234,5 @@ export class Travelpen {
     }
   }
 }
-
-let travelpen = new Travelpen()
 
 export default travelpen
